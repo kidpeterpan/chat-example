ab7ebd3cff9449d8e66dede5a383050b35670093
konghiran.w <konghiran.w@linecorp.com>
2021-07-08 13:48:09 +0700

    add emitting event and broadcasting event

--- index.js
+++ index.js
@@ -13,6 +13,13 @@ app.get('/', (req,res) => {
 // event connection
 io.on('connection', (socket) => {
     console.log('a user connected');
+    // event chat message
+    socket.on('chat message', (msg) => {
+       // broadcasting from the server to every client
+       io.emit('chat message', msg);
+       // when want to send to everyone except the socket owner
+       // socket.broadcast.emit('chat message', msg); 
+    });
     // event disconnect
     socket.on('disconnect',()=>{
         console.log('user disconnected');
